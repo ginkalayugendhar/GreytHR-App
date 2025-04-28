eab7e97203733dd534f90157beec707d812edc3d
yugendhar <ginkalayugendhar@gmail.com>
2025-04-28 16:16:07 +0530

    Delete README.md

---
+++ src/test/java/com/hg/greythr/GreytHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hg.greythr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreytHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
