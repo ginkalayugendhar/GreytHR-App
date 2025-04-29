793dcb5803cb7ea91d63c1572c69857b9b01191e
yugendhar <ginkalayugendhar@gmail.com>
2025-04-29 12:58:44 +0530

    Initial commit - Spring Boot and React app

---
+++ src/main/java/com/hg/greythr/GreytHrApplication.java
@@ -0,0 +1,13 @@
+package com.hg.greythr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreytHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreytHrApplication.class, args);
+	}
+
+}
